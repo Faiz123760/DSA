67c95ff4134d84a436db9282f36d0856ac39029e
Mohd Faiz <145976190+Faiz123760@users.noreply.github.com>
2025-09-29 23:52:17 +0530

    Time: 2 ms (96.87%) | Memory: 42.1 MB (31.83%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,23 @@
+import java.util.Stack;
+
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '{' || c == '[') {
+                stack.push(c); // push opening brackets
+            } else {
+                if (stack.isEmpty()) return false; // no opening for closing
+                char top = stack.pop();
+                if ((c == ')' && top != '(') || 
+                    (c == '}' && top != '{') || 
+                    (c == ']' && top != '[')) {
+                    return false; // mismatch found
+                }
+            }
+        }
+        
+        return stack.isEmpty(); // valid only if nothing left
+    }
+}
